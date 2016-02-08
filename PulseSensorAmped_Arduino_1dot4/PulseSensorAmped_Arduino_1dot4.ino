565a4dd24f65b5dbef30836f3cbf7370345aebbb
Morgante Pell <morgante.pell@morgante.net>
2016-02-08 16:23:17 +0400

    improve controller serial comm

--- PulseSensorAmped_Arduino_1dot4/PulseSensorAmped_Arduino_1dot4.ino
+++ PulseSensorAmped_Arduino_1dot4/PulseSensorAmped_Arduino_1dot4.ino
@@ -1,13 +1,24 @@
 //  Variables
-int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+int pulsePin = 2;                 // Pulse Sensor purple wire connected to analog pin 0
 int blinkPin = 13;                // pin to blink led at each beat
 int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
 int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
 
 // Flex variables
-int flexLeftPin = A1;
-int leftLedPin = 9;
-int flexMin = 700;
+int leftFlexMin = 850;
+int leftFlexPin = A1;
+int leftLedPin = 3;
+
+int rightFlexMin = 850;
+int rightFlexPin = A0;
+int rightLedPin = 5;
+
+// Buttons
+int jumpSwitchPin = 2;
+int jumpLedPin = 3;
+
+int stopSwitchPin = 4;
+int stopLedPin = 5;
 
 // Volatile Variables, used in the interrupt service routine!
 volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
@@ -17,12 +28,19 @@ volatile boolean Pulse = false;     // "True" when User's live heartbeat is dete
 volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
 
 // Regards Serial OutPut  -- Set This Up to your needs
-static boolean serialVisual = false;   // Set to 'false' by Default.  Re-set to 'true' to see Arduino Serial Monitor ASCII Visual Pulse 
+static boolean serialVisual = true;   // Set to 'false' by Default.  Re-set to 'true' to see Arduino Serial Monitor ASCII Visual Pulse 
 
 
 void setup(){
 
   pinMode(leftLedPin, OUTPUT);
+  pinMode(rightLedPin, OUTPUT);
+
+  pinMode(jumpSwitchPin, INPUT);
+  pinMode(jumpLedPin, OUTPUT);
+
+  pinMode(stopSwitchPin, INPUT);
+  pinMode(stopLedPin, OUTPUT);  
   
   pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
   pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
@@ -36,7 +54,40 @@ void setup(){
 
 //  Where the Magic Happens
 void loop(){
-  digitalWrite(leftLedPin, HIGH);
+  int jumpSwitchState = !digitalRead(jumpSwitchPin);
+
+  if (jumpSwitchState) {
+    digitalWrite(jumpLedPin, HIGH);
+  } else {
+    digitalWrite(jumpLedPin, LOW);
+  }
+
+  int stopSwitchState = !digitalRead(stopSwitchPin);
+
+  if (stopSwitchState) {
+    digitalWrite(stopLedPin, HIGH);
+  } else {
+    digitalWrite(stopLedPin, LOW);
+  }
+
+  // left & right handler
+  int leftVal = analogRead(leftFlexPin);
+//  Serial.println(leftVal);
+
+  if (leftVal >= leftFlexMin) {
+    digitalWrite(leftLedPin, HIGH);
+    delay(500);
+    digitalWrite(leftLedPin, LOW);
+  }
+
+  int rightVal = analogRead(rightFlexPin);
+//  Serial.println(rightVal);
+
+  if (rightVal >= rightFlexMin) {
+    digitalWrite(rightLedPin, HIGH);
+    delay(500);
+    digitalWrite(rightLedPin, LOW);
+  }
   
     serialOutput() ;       
     
@@ -49,6 +100,8 @@ void loop(){
         serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.     
         QS = false;                      // reset the Quantified Self flag for next time    
   }
+
+  digitalWrite(blinkPin, LOW);
      
   delay(20);                             //  take a break
 }
